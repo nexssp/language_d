a4d6c5858a472cd00756b6f91daee7732351fdfc
Marcin Polak <mapoart@gmail.com>
2020-09-22 14:26:34 +0200

    Added functions for different distros

--- d.linux.nexss.config.js
+++ d.linux.nexss.config.js
@@ -1,5 +1,7 @@
 let languageConfig = Object.assign({}, require("./d.win32.nexss.config"));
 
+const os = require("@nexssp/os");
+
 languageConfig.compilers = {
   dmd: {
     install: "curl https://dlang.org/install.sh | bash -s",
@@ -8,5 +10,10 @@ languageConfig.compilers = {
     help: ``,
   },
 };
+
+// Below is unnecessary
+languageConfig.compilers.dmd.install = os.replacePMByDistro(
+  languageConfig.compilers.dmd.install
+);
 
 module.exports = languageConfig;
