79d736161c01cb881649f08e9883f51bfe23ce67
Marcin Polak <mapoart@gmail.com>
2020-09-22 18:11:11 +0200

    Added error/solution

--- nexss.d.errors.js
+++ nexss.d.errors.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "Cannot find module '(.*?)'": "nexss js install <package>"
+  "Run `source (?<found1>.*?)\\)` in your shell to use (?<found2>.*?)\\).": `to enable d language run 'source <found1>'`,
 };
