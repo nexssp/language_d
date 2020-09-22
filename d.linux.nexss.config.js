2b4c737403a654705ffadc78d51cb1a416ddd9a5
Marcin Polak <mapoart@gmail.com>
2020-09-22 14:43:21 +0200

    Old version works better

--- d.linux.nexss.config.js
+++ d.linux.nexss.config.js
@@ -1,7 +1,5 @@
 let languageConfig = Object.assign({}, require("./d.win32.nexss.config"));
 
-const os = require("@nexssp/os");
-
 languageConfig.compilers = {
   dmd: {
     install: "curl https://dlang.org/install.sh | bash -s",
@@ -10,10 +8,5 @@ languageConfig.compilers = {
     help: ``,
   },
 };
-
-// Below is unnecessary however you can use it later
-languageConfig.compilers.dmd.install = os.replacePMByDistro(
-  languageConfig.compilers.dmd.install
-);
 
 module.exports = languageConfig;
